0df34601cc7b34641515a2d33f0fa2e955a442a1
masedki <masedki@gmail.com>
2014-08-07 15:15:28 +0200

    NULL dans CritClust.cpp

--- src/critClust.cpp
+++ src/critClust.cpp
@@ -64,7 +64,7 @@ List CritClust::ClustBestModel(vector<int> numExp)
          NumericVector critvalues(bestResult.slot("criterionValue"));
          return List::create(Named("criterionValue") = -critvalues[0],
                              Named("model") = bestResult.slot("model"),
-                             Named("proba") = R_NilValue, 
+                             Named("proba") = 0, 
                              Named("criterion") = "BIC",
                              Named("nbCluster") = bestResult.slot("nbCluster"),
                              Named("partition") = bestResult.slot("partition"));
